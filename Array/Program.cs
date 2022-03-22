42f7ac5079627eabe409c290055c565aa6968215
Frederik Hvid Sørensen <fred09j3@zbc.dk>
2022-03-22 11:53:31 +0100

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,14 @@
+﻿byte[] array = new byte[9];
+
+for (int i = 0; i < 9; i++)
+{
+    array[i] = (byte)(i + 1);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == 5)
+    {
+        array[i] *= 2;
+    }
+}
